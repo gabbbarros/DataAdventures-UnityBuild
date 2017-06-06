e1c745ccbc176c2db1f8214da4fbb2da4e2cbb1b
Michael Green <mcg520@nyu.edu>
2017-06-05 23:20:09 -0400

    Added Tab Manager

---
+++ Assets/Scripts/GameFileReader/FileReader.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+
+public class FileReader {
+
+    public string FileName{get;set;}
+
+    // Reads a SaveGame file. SaveGame files are saved in JSON format
+    public FileReader()
+    {
+
+
+    }
+    public void ReadSaveFile()
+    {
+
+    }
+    /*
+     * People are Objects with values in JSON. They have a Name, an ID, a DialogueTree,
+     *
+     */
+    public void ReadPeople()
+    {
+        // loop through all the people
+
+    }
+
+    public void ReadPlaces()
+    {
+
+    }
+
+    public void ReadBuildings()
+    {
+
+
+    }
+
+    public void ReadConditions()
+    {
+
+    }
+
+    public void ReadSuspectList()
+    {
+
+
+    }
+
+    public void ReadCulprit()
+    {
+
+    }
+
+    public void ReadPerson(string personString)
+    {
+        // Read Name
+
+    }
+
+    public void ReadName()
+    {
+
+    }
+}
