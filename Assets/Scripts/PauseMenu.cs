291770df41e945e34569279c38e377908628c3db
Michael Green <mcg520@nyu.edu>
2017-09-26 17:07:10 -0400

    Update to 2017.1.1
    Also added Kathy’s main menu and KathyScene

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+    public Transform canvas;
+
+    public GameObject Menu;
+    public GameObject SettingsMenu;
+
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Menu.gameObject.activeInHierarchy == false)
+            {
+                Menu.gameObject.SetActive(true);
+            }
+            else
+            {
+                Menu.gameObject.SetActive(false);
+
+            }
+
+        }
+	}
+
+    public void Resume ()
+    {
+        Menu.gameObject.SetActive(false);
+  
+    }
+    public void ReturnToMainMenu ()
+    {
+        
+    }
+    public void Settings ()
+    {
+        Menu.gameObject.SetActive(false);
+        SettingsMenu.gameObject.SetActive(true); 
+    }
+    public void SettingsOkay ()
+    {
+        SettingsMenu.gameObject.SetActive(false);
+        Menu.gameObject.SetActive(true);
+    }
+    public void SettingsCancel ()
+    {
+        SettingsMenu.gameObject.SetActive(false);
+        Menu.gameObject.SetActive(true); 
+    }
+
+	public void Pause()
+	{
+
+        Menu.gameObject.SetActive(true);
+	}
+}
