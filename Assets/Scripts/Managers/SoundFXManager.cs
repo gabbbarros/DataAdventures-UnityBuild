b2368c7462caa2dc5267ded6de6fc082bfadd0e5
Michael Green <mcg520@nyu.edu>
2017-10-03 17:37:37 -0400

    Added Sound files and SoundFXManager

---
+++ Assets/Scripts/Managers/SoundFXManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundFXManager : MonoBehaviour {
+
+	public List<AudioClip> sounds;
+	public AudioSource soundJukebox;
+	public static SoundFXManager instance = null;
+
+	void Awake()
+	{
+		// check to see if an instance exists
+		if (instance == null)
+		{
+			// if not set it to this
+			instance = this;
+		}
+	}
+	public void PlaySingle(int clipID)
+	{
+		// set clip to whatever we want to play
+		soundJukebox.clip = sounds[clipID];
+
+		// play clip
+		soundJukebox.Play();
+	}
+}
